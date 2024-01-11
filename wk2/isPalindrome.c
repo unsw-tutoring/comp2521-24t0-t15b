243080387f5698f7e4870d8ab806fb3f2dce91dc
Josh <joshconstantinelim@gmail.com>
2024-01-11 12:20:54 +1100

    wk2

---
+++ wk2/isPalindrome.c
@@ -0,0 +1,10 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+// Given a string, return true if the string is a palindrome.
+bool isPalindrome(char *str) {
+    // TODO
+    return false;
+}
---
+++ wk2/twoSum.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+// Given an array of integers, return true if there exists two numbers in the array such that their sum is equal to the target.
+bool twoSum(int *arr, int size, int target) {
+    // TODO
+    return false;
+}
