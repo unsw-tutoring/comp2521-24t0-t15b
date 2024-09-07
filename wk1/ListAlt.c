8c7e861ff92a4aa5b863f9341341871d3154a5e5
Josh <joshconstantinelim@gmail.com>
2024-09-07 18:35:28 +1000

    wk1: listAlt

--- wk1/ListAlt.c
+++ wk1/ListAlt.c
@@ -15,10 +15,28 @@ struct list {
     // struct node *tail;
 };
 
+int doListLength(struct node *n) {
+    if (n == NULL) return 0;
+    return 1 + doListLength(n->next);
+}
+
 // returns the length of a linked list
 int listLength(struct list *l) {
-    // TODO
-    return 0;
+    return doListLength(l->head);
+}
+
+struct node *doListDelete(struct node *n, int val) {
+    if (n == NULL) return NULL;
+
+	// if value is at head of list, delete it and return second node since it's the new head
+	if (n->data == val) {
+		struct node *newHead = n->next;
+		free(n);
+		return newHead;
+	}
+
+	n->next = doListDelete(n->next, val);
+	return n;
 }
 
 // deletes the first node with value 'val' from list
@@ -26,5 +44,5 @@ int listLength(struct list *l) {
 // .. note that we don't need to return the new head of the linked list, since
 // .. if that changes, we just update the l->head pointer
 void listDelete(struct list *l, int val) {
-    // TODO
+    l->head = doListDelete(l->head, val);
 }
